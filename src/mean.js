4a685402314506cfb6e839456c1e93520d79a198
atlassubbed <atlassubbed@gmail.com>
2018-07-15 22:39:14 -0400

    adds short circuits for edge cases

--- src/mean.js
+++ src/mean.js
@@ -1,3 +1,7 @@
 const sum = require("atlas-sum");
 
-module.exports = arr => sum(arr)/arr.length;
+module.exports = arr => {
+  const n = arr.length;
+  // short circuit all edge cases
+  return n ? n === 1 ? arr[0] : sum(arr)/n : void 0;
+}
--- test/mean.test.js
+++ test/mean.test.js
@@ -6,4 +6,10 @@ describe("mean", function(){
   it("should calculate the expectation value of the array", function(){
     expect(mean([1,2,3,4,5,6,7,8,9,10])).to.equal(5.5)
   })
+  it("should return undefined for a 0-array", function(){
+    expect(mean([])).to.be.undefined
+  })
+  it("should return the only element for a 1-array", function(){
+    expect(mean([6])).to.equal(6)
+  })
 })
